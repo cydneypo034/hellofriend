86603157966be64f6ed07255c0d4fef7e2d145d6
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-25 14:23:10 -0500

    added new code for Javascript clock

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 //clock creation
-
-function startTime() {
-    const todayDate = new Date();
-    const hour = todayDate.getHours();
-    const minute = todayDate.getMinutes();
-    const second = todayDate.getSeconds();
-
-    document.getElementById('clock').innerHTML = hour + ':' + minute + ':' + second;
+function startTime() {    
     setTimeout(startTime, 1000)
 
+    let todayDate = new Date();
+    let hour = todayDate.getHours();
+    let minute = todayDate.getMinutes();
+    let second = todayDate.getSeconds();
+    am_pm = "AM";
+
+    if(hour > 12){
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if(hour == 0) {
+        hour = 12;
+        am_pm = "AM";
+    }
+    
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+
+    document.getElementById('clock').innerHTML = 
+    hour + ':' + minute + ':' + second;
 }
+
 
 startTime();

659dbda5ceaf3ecd3a8a969c1ada4751bf53a408
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-26 10:20:41 -0500

    added javascript code for gifs to change with time

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+//gif id
+let gifSlides = document.getElementById("slidedeck");
+//gif array
+let gifArray = ["gif-files/totoro-forestgif.gif", "gif-files/ponyo-seagif.gif", "gif-files/howlsmovingcastle-floatinggif.gif",
+"gif-files/spiritedaway-traingif.gif", "gif-files/princessmono-warriorgif.gif", "gif-files/kikis-delivery-service.gif", "gif-files/castle-in-the-sky.gif",
+"gif-files/grave-of-the-fireflies-grave.gif", "gif-files/only-yesterdaygif.gif", "gif-files/whisperoftheheart-gif.gif",
+"gif-files/the-wind-rises-studio-ghibli.gif", "gif-files/the-cat-returns.gif"];
+//gif index to set for array
+let gifIndex = 0;
+
+
 //clock creation
 function startTime() {    
     setTimeout(startTime, 1000)
@@ -16,7 +27,7 @@ function startTime() {
         hour = 12;
         am_pm = "AM";
     }
-    
+
     hour = hour < 10 ? "0" + hour : hour;
     minute = minute < 10 ? "0" + minute : minute;
     second = second < 10 ? "0" + second : second;
@@ -24,6 +35,35 @@ function startTime() {
     document.getElementById('clock').innerHTML = 
     hour + ':' + minute + ':' + second;
 }
-
-
 startTime();
+
+function changeGif() {
+    gifSlides.setAttribute("src", gifArray[gifIndex]);
+    gifIndex++;
+    if(gifIndex >= gifArray.length) {
+        gifIndex = 0;
+    }
+}
+
+function gifsAndTime() {
+    let todayDate = new Date();
+    let hour = todayDate.getHours();
+
+    if(hour >= 1) {
+        gifSlides.setAttribute("src", gifArray[0])
+    }
+    else if(hour >= 2) {
+        gifSlides.setAttribute("src", gifArray[1]);
+    }
+    else if(hour >= 3) {
+        gifSlides.setAttribute("src", )
+    }
+    else {
+        let intervalHandle = setInterval(changeGif, 3000)
+    }
+    gifSlides.onclick = function() {
+        clearInterval(intervalHandle);
+    }
+}
+
+gifsAndTime();

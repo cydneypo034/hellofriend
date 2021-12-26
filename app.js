f7c33a0b0076ce5bedd403e719f3e0c403816869
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-26 13:38:36 -0500

    added new audio files for each gif and new array for audios

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ let gifArray = ["gif-files/totoro-forestgif.gif", "gif-files/ponyo-seagif.gif", 
 //gif index to set for array
 let gifIndex = 0;
 
+//audio id
+let audioPicks = document.getElementById("audio")
+//audio files from https://downloads.khinsider.com/game-soundtracks/album/studio-ghibli-soundtrack-collection-bonus-disc
+let audioArray = ["audio-files/10 - totoro.mp3", "audio-files/06 - Ponyo and Sosuke.mp3", "audio-files/03 sky stroll.mp3", 
+"audio-files/10 ano hi no kawa (spirited away).mp3", "audio-files/05 Eto (Land Of The Impure).mp3", "audio-files/03 a town with an ocean view (kiki's delivery service).mp3",
+"audio-files/01 - the girl who fell from the sky.mp3", "audio-files/02 Drama Genki Ni Naresou.mp3", "audio-files/01 Le Temps Des Cerises.mp3",
+"audio-files/01 - oka no machi.mp3", "audio-files/01 A Journey (A Dream of Flight).mp3", "audio-files/04 Sora Iro No Tane.mp3" ]
+//audio index to set for array
+let audioIndex = 0;
+
 
 //clock creation
 function startTime() {    
@@ -45,69 +55,76 @@ function changeGif() {
     }
 }
 
-    let totoroAudio = document.getElementById("totoro-audio");
-    let ponyoAudio = document.getElementById("ponyo-audio");
-    let howlsAudio = document.getElementById("howlsmovingcastle-audio");
-    let spiritedAwayAudio = document.getElementById("spiritedaway-audio");
-    let princessMonoAudio = document.getElementById("princessmono-audio");
-    let kikiDeliveryAudio = document.getElementById("kikisdelivery-audio");
-    let castleInTheSkyAudio = document.getElementById("castleinthesky-audio");
-    let graveOfFirefliesAudio = document.getElementById("graveoffireflies-audio");
-    let onlyYesterdayAudio = document.getElementById("onlyyesterday-audio");
-    let whisperOfTheHeart = document.getElementById("whisperoftheheart-audio");
-    let windRisesAudio = document.getElementById("thewindrises-audio");
-    let catReturnsAudio = document.getElementById("thecatreturns-audio");
+function changeAudio() {
+    audioPicks.setAttribute("src", audioArray[audioIndex]);
+    audioIndex++;
+    if(audioIndex >= audioArray.length) {
+        arrayIndex = 0;
+    }
+}
+
 
 function gifsAndTime() {
     let todayDate = new Date();
     let hour = todayDate.getHours();
 
     if(hour <= 1) {
-        gifSlides.setAttribute("src", gifArray[0]);
+        gifSlides.setAttribute("src", gifArray[0]);    
         totoroAudio.play();
     }
     else if(hour <= 2) {
-        gifSlides.setAttribute("src", gifArray[1]);
+        gifSlides.setAttribute("src", gifArray[1]);    
+        let ponyoAudio = document.getElementById("ponyo-audio");
         ponyoAudio.play();
     }
     else if(hour <= 3) {
-        gifSlides.setAttribute("src", gifArray[2]);
+        gifSlides.setAttribute("src", gifArray[2]);    
+        let howlsAudio = document.getElementById("howlsmovingcastle-audio");
         howlsAudio.play();
     }
     else if(hour <= 4) {
         gifSlides.setAttribute("src", gifArray[3]);
+        let spiritedAwayAudio = document.getElementById("spiritedaway-audio");
         spiritedAwayAudio.play();
     }
     else if(hour <= 5) {
         gifSlides.setAttribute("src", gifArray[4]);
+        let princessMonoAudio = document.getElementById("princessmono-audio");
         princessMonoAudio.play();
     }
     else if(hour <= 6) {
         gifSlides.setAttribute("src", gifArray[5]);
+        let kikiDeliveryAudio = document.getElementById("kikisdelivery-audio");
         kikiDeliveryAudio.play();
     }
     else if(hour <= 7) {
         gifSlides.setAttribute("src", gifArray[6]);
+        let castleInTheSkyAudio = document.getElementById("castleinthesky-audio");
         castleInTheSkyAudio.play();
     }
     else if(hour <= 8) {
         gifSlides.setAttribute("src", gifArray[7]);
+        let graveOfFirefliesAudio = document.getElementById("graveoffireflies-audio");
         graveOfFirefliesAudio.play();
     }
     else if(hour <= 9) {
         gifSlides.setAttribute("src", gifArray[8]);
+        let onlyYesterdayAudio = document.getElementById("onlyyesterday-audio");
         onlyYesterdayAudio.play();
     }
     else if(hour <= 10) {
         gifSlides.setAttribute("src", gifArray[9]);
+        let whisperOfTheHeart = document.getElementById("whisperoftheheart-audio");
         whisperOfTheHeart.play();
     }
     else if(hour <= 11) {
         gifSlides.setAttribute("src", gifArray[10]);
+        let windRisesAudio = document.getElementById("thewindrises-audio");
         windRisesAudio.play();
     }
     else if(hour <= 12) {
         gifSlides.setAttribute("src", gifArray[11]);
+        let catReturnsAudio = document.getElementById("thecatreturns-audio");
         catReturnsAudio.play();
     }
     else {

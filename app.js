9aed930e30d911aa5a74619cd7b3ba43aaf9544c
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-26 16:56:23 -0500

    added code back for audio and gif code for javascript

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let gifArray = ["gif-files/totoro-forestgif.gif", "gif-files/ponyo-seagif.gif", 
 let gifIndex = 0;
 
 //audio id
-let audioPicks = document.getElementById("audio");
+let audioPicks = document.getElementById("audioclip");
 //audio files from https://downloads.khinsider.com/game-soundtracks/album/studio-ghibli-soundtrack-collection-bonus-disc
 let audioArray = ["audio-files/10 - totoro.mp3", "audio-files/06 - Ponyo and Sosuke.mp3", "audio-files/03 sky stroll.mp3", 
 "audio-files/10 ano hi no kawa (spirited away).mp3", "audio-files/05 Eto (Land Of The Impure).mp3", "audio-files/03 a town with an ocean view (kiki's delivery service).mp3",
@@ -44,14 +44,70 @@ function startTime() {
 
     document.getElementById('clock').innerHTML = 
     hour + ':' + minute + ':' + second;
+
+    
 }
 startTime();
+
 
 function changeGif() {
     gifSlides.setAttribute("src", gifArray[gifIndex]);
     gifIndex++;
     if(gifIndex >= gifArray.length) {
         gifIndex = 0;
+    }
+}
+
+
+function gifsAndTime() {
+    let todayDate = new Date();
+    let hour = todayDate.getHours();
+
+    if(hour == 0) {
+        let gifIndex = null;
+        console.log("no gif", gifIndex)
+    }
+    else if(hour >= 1) {
+        gifSlides.setAttribute("src", gifArray[0]);
+    }
+    else if(hour >= 2) {
+        gifSlides.setAttribute("src", gifArray[1]);
+    }
+    else if(hour >= 3) {
+        gifSlides.setAttribute("src", gifArray[2]);
+    }
+    else if(hour >= 4) {
+        gifSlides.setAttribute("src", gifArray[3]);
+    }
+    else if(hour >= 5) {
+        gifSlides.setAttribute("src", gifArray[4]);
+    }
+    else if(hour >= 6) {
+        gifSlides.setAttribute("src", gifArray[5]);
+    }
+    else if(hour >= 7) {
+        gifSlides.setAttribute("src", gifArray[6]);
+    }
+    else if(hour >= 8) {
+        gifSlides.setAttribute("src", gifArray[7]);
+    }
+    else if(hour >= 9) {
+        gifSlides.setAttribute("src", gifArray[8]);
+    }
+    else if(hour >= 10) {
+        gifSlides.setAttribute("src", gifArray[9]);
+    }
+    else if(hour >= 11) {
+        gifSlides.setAttribute("src", gifArray[10]);
+    }
+    else if(hour >= 12) {
+        gifSlides.setAttribute("src", gifArray[11]);
+    }
+    else {
+        let intervalHandle = setInterval(changeGif, 3000)
+    }
+    gifSlides.onclick = function() {
+        clearInterval(intervalHandle);
     }
 }
 
@@ -63,136 +119,56 @@ function changeAudio() {
     }
 }
 
-
-function pick1() {
+function audioAndTime() {
     let todayDate = new Date();
     let hour = todayDate.getHours();
 
-    if(hour <= 1) {
-        gifSlides.setAttribute("src", gifArray[0]);    
+    if(hour == 0) {
+        let audioIndex = null;
+        console.log("no audio", audioIndex)
+    }
+    else if(hour >= 1) {
         audioPicks.setAttribute("src", audioArray[0]);
     }
-}
-
-function pick2() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-
-    if(hour <= 2) {
-        gifSlides.setAttribute("src", gifArray[1]);    
+    else if(hour >= 2) {
         audioPicks.setAttribute("src", audioArray[1]);
     }
-}
-
-function pick3() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-
-    if(hour <= 3) {
-        gifSlides.setAttribute("src", gifArray[2]);    
+    else if(hour >= 3) {
         audioPicks.setAttribute("src", audioArray[2]);
     }
-}
-
-function pick4() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    
-    if(hour <= 4) {
-        gifSlides.setAttribute("src", gifArray[3]);
+    else if(hour >= 4) {
         audioPicks.setAttribute("src", audioArray[3]);
     }
-}
-
-function pick5() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    
-    if(hour <= 5) {
-        gifSlides.setAttribute("src", gifArray[4]);
+    else if(hour >= 5) {
         audioPicks.setAttribute("src", audioArray[4]);
     }
-}
-
-function pick6() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    if(hour <= 6) {
-        gifSlides.setAttribute("src", gifArray[5]);
+    else if(hour >= 6) {
         audioPicks.setAttribute("src", audioArray[5]);
     }
-}
-
-function pick7() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    if(hour <= 7) {
-        gifSlides.setAttribute("src", gifArray[6]);
+    else if(hour >= 7) {
         audioPicks.setAttribute("src", audioArray[6]);
     }
-}
-    
-function pick8() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    if(hour <= 8) {
-        gifSlides.setAttribute("src", gifArray[7]);
+    else if(hour >= 8) {
         audioPicks.setAttribute("src", audioArray[7]);
     }
-}
-
-function pick9() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    if(hour <= 9) {
-        gifSlides.setAttribute("src", gifArray[8]);
+    else if(hour >= 9) {
         audioPicks.setAttribute("src", audioArray[8]);
     }
-}
-    
-function pick10() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    if(hour <= 10) {
-        gifSlides.setAttribute("src", gifArray[9]);
+    else if(hour >= 10) {
         audioPicks.setAttribute("src", audioArray[9]);
     }
-}
-    
-function pick11() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    if(hour <= 11) {
-        gifSlides.setAttribute("src", gifArray[10]);
+    else if(hour >= 11) {
         audioPicks.setAttribute("src", audioArray[10]);
     }
-}
-
-function pick12() {
-    let todayDate = new Date();
-    let hour = todayDate.getHours();
-    if(hour <= 12) {
-        gifSlides.setAttribute("src", gifArray[11]);
+    else if(hour >= 12) {
         audioPicks.setAttribute("src", audioArray[11]);
     }
-}
-
-/*
-function gifAndTimes() {
-    {
-        let intervalHandle = setInterval(changeGif, 3000)
-        let secondInterval = setInterval(changeAudio, 3000)
-    }
-    gifSlides.onclick = function() {
-        clearInterval(intervalHandle);
+    else {
+        let intervalHandle2 = setInterval(changeAudio, 3000)
     }
     audioPicks.onclick = function() {
-        clearInterval(secondInterval);
-    } 
+        clearInterval(intervalHandle2);
+    }
 }
-    
-*/
-
 gifsAndTime();
-pick1();
-pick2();
+audioAndTime();

97209d47c725e2da46317597e653677ae90e0f8d
Cydney Pollard <sincerelycydney087@gmail.com>
2023-08-08 18:08:06 -0400

    removed ampitude.js and will create music player through javascript; layout of clock project will change to add a music player under the clock

--- app.js
+++ app.js
@@ -1,91 +1,54 @@
 // list of songs
 const songs = [
     {
-        "title": "oka no machi",
-        "song": "audio-files/01 - oka no machi.mp3"
+        "name": "oka no machi",
+        "url": "audio-files/01 - oka no machi.mp3"
     },
     {
-        "title": "the girl who fell from the sky",
-        "song": "audio-files/01 - the girl who fell from the sky.mp3"
+        "name": "the girl who fell from the sky",
+        "url": "audio-files/01 - the girl who fell from the sky.mp3"
     },
     {
-        "title": "A Journey (A Dream of Flight)",
-        "song": "audio-files/01 A Journey (A Dream of Flight).mp3"
+        "name": "A Journey (A Dream of Flight)",
+        "url": "audio-files/01 A Journey (A Dream of Flight).mp3"
     },
     {
-        "title": "Le Temps Des Cerises",
-        "song": "audio-files/01 Le Temps Des Cerises.mp3"
+        "name": "Le Temps Des Cerises",
+        "url": "audio-files/01 Le Temps Des Cerises.mp3"
     },
     {
-        "title": "Drama Genki Ni Naresou",
-        "song": "audio-files/02 Drama Genki Ni Naresou.mp3"
+        "name": "Drama Genki Ni Naresou",
+        "url": "audio-files/02 Drama Genki Ni Naresou.mp3"
     },
     {
-        "title": "a town with an ocean view (kiki's delivery service)",
-        "song": "audio-files/03 a town with an ocean view (kiki's delivery service).mp3"
+        "name": "a town with an ocean view (kiki's delivery service)",
+        "url": "audio-files/03 a town with an ocean view (kiki's delivery service).mp3"
     },
     {
-        "title": "sky stroll",
-        "song": "audio-files/03 sky stroll.mp3"
+        "name": "sky stroll",
+        "url": "audio-files/03 sky stroll.mp3"
     },
     {
-        "title": "Sora Iro No Tane",
-        "song": "audio-files/04 Sora Iro No Tane.mp3"
+        "name": "Sora Iro No Tane",
+        "url": "audio-files/04 Sora Iro No Tane.mp3"
     },
     {
-        "title": "Eto (Land Of The Impure)",
-        "song": "audio-files/05 Eto (Land Of The Impure).mp3"
+        "name": "Eto (Land Of The Impure)",
+        "url": "audio-files/05 Eto (Land Of The Impure).mp3"
     },
     {
-        "title": "Ponyo and Sosuke",
-        "song": "audio-files/06 - Ponyo and Sosuke.mp3"
+        "name": "Ponyo and Sosuke",
+        "url": "audio-files/06 - Ponyo and Sosuke.mp3"
     },
     {
-        "title": "Totoro",
-        "song": "audio-files/10 - totoro.mp3"
+        "name": "Totoro",
+        "url": "audio-files/10 - totoro.mp3"
     },
     {
-        "title": "ano hi no kawa (spirited away)",
-        "song": "audio-files/10 ano hi no kawa (spirited away).mp3"
+        "name": "ano hi no kawa (spirited away)",
+        "url": "audio-files/10 ano hi no kawa (spirited away).mp3"
     }
 ]
-const audio = document.getElementById('audio');
-const title = document.getElementById('title');
-const songsPlayed = [];
-let songIndex = 2;
-
-//function to play song in order of array
-function musicPlayer() {
-    const musicContainer = document.getElementById('music-container');
-    for(let i = 0; i < songs.length; i++){
-       const songholder = document.createElement("span");
-       songholder.setAttribute("data-id", i);
-       musicContainer.appendChild(songholder);
-    }
-}
-
-musicPlayer();
-
-loadSong(songs[songIndex]);
-function loadSong(song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-  }
-
-// function playFunctionality() {
-//     const audioId = this.getAttribute('data-id');
-//     console.log("Song Playing: " + songs[audioId].title)
-//     title.innerHTML = "Song Playing: " + songs[audioId].title
-//     //songsPlayed.push(songs[songId].title)
-//     this.setAttribute("audio", songs[audioId].song);
-//     if(songsPlayed.length == 1){
-//         audio.play();
-//         songsPlayed = [];
-//     }
-// }
-
-//setSong.innerHTML = "Song Playing: " + song[songId].title
-//if song[songId].song == true { playSound() }
 
 //clock creation
 function startTime() {    

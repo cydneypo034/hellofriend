82a6a7850eb823a0c021c5a206ab931e9d1d47b6
Cydney Pollard <sincerelycydney087@gmail.com>
2023-08-10 06:50:28 -0400

    created seek slider function to check if current track duration is actual number, set calculations for how much time would be left and update duration

--- app.js
+++ app.js
@@ -151,6 +151,33 @@ function previousSong() {
     playSong();
 }
 
+//updating the seek slider
+function seekUpdate() {
+    let seekPosition = 0;
+
+    //check if current track duration is a legible number
+    if(!isNaN(curr_song.duration)){
+        seekPosition = curr_song.currentTime * (100 / curr_song.duration);
+        seek_slider.value = seekPosition;
+
+        //Calculate the time left and the total duration
+        let currentMinutes = Math.floor(curr_song.currentTime / 60);
+        let currentSeconds = Math.floor(curr_song.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_song.duration / 60);
+        let durationSeconds = Math.floor(curr_song.duration - durationMinutes * 60);
+
+        // Add a zero to the single digit time values
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        // Display the updated duration
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    }
+}
+
 loadSong(song_index);
 
 // ------------------ DARK AND LIGHT MODE//
